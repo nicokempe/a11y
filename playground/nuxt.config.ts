d833286ade6773d3fd6eb8694e0a6f2fc9c5bfdf
Daniel Roe <daniel@roe.dev>
2024-12-12 13:30:22 +0000

    test: add basic e2e assertion + bump dev deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['@nuxt/a11y'],
   devtools: { enabled: true },
   compatibilityDate: '2024-08-21',
   a11y: {},
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,15 +1,43 @@
 import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils'
+import { setup, createPage, url } from '@nuxt/test-utils/e2e'
 
 describe('ssr', async () => {
   await setup({
+    dev: true,
     rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+    browser: true,
   })
 
   it('renders the index page', async () => {
-    // Get response to a server-rendered page with `$fetch`.
-    const html = await $fetch('/')
-    expect(html).toContain('Nuxt module playground')
+    const page = await createPage()
+    const logs: string[] = []
+    page.on('console', (msg) => {
+      logs.push(msg.text())
+    })
+    await page.goto(url('/'), { waitUntil: 'hydration' })
+    expect(logs.filter(l => !l.startsWith('[vite]') && !l.startsWith('<Suspense>'))).toMatchInlineSnapshot(`
+      [
+        "%ca11y%c Documents must have <title> element to aid in navigation
+        https://dequeuniversity.com/rules/axe/4.10/document-title?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #f25c54; ",
+        "%ca11y%c Heading levels should only increase by one
+        https://dequeuniversity.com/rules/axe/4.10/heading-order?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #cf863e;  JSHandle@node",
+        "%ca11y%c <html> element must have a lang attribute
+        https://dequeuniversity.com/rules/axe/4.10/html-has-lang?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #f25c54; ",
+        "%ca11y%c Document should have one main landmark
+        https://dequeuniversity.com/rules/axe/4.10/landmark-one-main?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #cf863e; ",
+        "%ca11y%c Page should contain a level-one heading
+        https://dequeuniversity.com/rules/axe/4.10/page-has-heading-one?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #cf863e; ",
+        "%ca11y%c All page content should be contained by landmarks
+        https://dequeuniversity.com/rules/axe/4.10/region?application=axeAPI
+       color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: #cf863e;  JSHandle@node",
+      ]
+    `)
+    await page.close()
   })
 })
